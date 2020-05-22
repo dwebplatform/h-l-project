9c45ed5c3709eb8b503464249df62beb7b22b7b2
Vladimir <karpov-vb-1996@mail.ru>
2020-05-22 21:48:07 +0300

    adding react-animation-example

---
+++ react-examples/app.js
@@ -0,0 +1,100 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useSprings, animated } from "react-spring";
+import swap from "lodash-move";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const TWO_PI = Math.PI * 2;
+
+const circlesWithCoords = [
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  },
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  },
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  },
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  },
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  },
+  {
+    color: "green",
+    x: 0,
+    y: 0
+  }
+];
+circlesWithCoords.forEach((item, i) => {
+  item.x = 150 * Math.sin(((i + 1) * TWO_PI) / circlesWithCoords.length);
+  item.y = -150 * Math.cos(((i + 1) * TWO_PI) / circlesWithCoords.length);
+});
+console.log(circlesWithCoords);
+
+function App() {
+  const DefaultsCircles = [
+    "teal",
+    "yellow",
+    "blue",
+    "orange",
+    "purple",
+    "skyblue"
+  ];
+  const [circles, setCircles] = useState(circlesWithCoords);
+
+  const [{ x, y }, setXY] = useState({ x: 0, y: 0 });
+
+  const cirlcesRef = useRef(circles);
+
+  useEffect(() => {
+    let newOrderCircles = [...cirlcesRef.current];
+    newOrderCircles = swap(newOrderCircles, 0, 3);
+    cirlcesRef.current = newOrderCircles;
+    setCircles(s => {
+      return [...newOrderCircles];
+    });
+  }, []);
+
+  const handleMouseMove = e => {
+    setXY({
+      x: Math.floor((e.clientX / window.innerWidth) * 255),
+      y: Math.floor((e.clientY / window.innerHeight) * 255)
+    });
+  };
+  const [time, setTime] = useState(0);
+  return (
+    <div className="container" onMouseMove={handleMouseMove}>
+      <div className="circle-container">
+        {circles.map((item, i) => {
+          return (
+            <div
+              className="circle"
+              key={i}
+              style={{
+                top: `${item.x}px`,
+                left: `${item.y}px`
+              }}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
